6b18e481dbece9bb21749704cb79968e744d047f
Qais Zaheb <100469755+QZaheb@users.noreply.github.com>
2022-06-30 01:31:10 +0430

    Add files via upload

---
+++ Navigate ths DOM - Children/app.js
@@ -0,0 +1,10 @@
+let qais = document.querySelector('#rsult');
+let zaheb = qais.childNodes;
+console.log(zaheb);
+
+
+let children = rsult.children;
+console.log(children);
+
+console.log(rsult.firstChild);
+console.log(rsult.lastChild);
